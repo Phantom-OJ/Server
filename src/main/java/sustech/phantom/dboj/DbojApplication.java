83d5360ea201c925bd0a64edf07111118396ac9e
lethal233 <leemdragon233@yahoo.com>
2020-10-01 21:43:10 +0800

    init

---
+++ src/main/java/sustech/phantom/dboj/DbojApplication.java
@@ -0,0 +1,13 @@
+package sustech.phantom.dboj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbojApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sustech/phantom/dboj/DbojApplicationTests.java
@@ -0,0 +1,13 @@
+package sustech.phantom.dboj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbojApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
