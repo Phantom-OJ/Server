3bd376a9e61963678fef6339900d86d237341645
lethal233 <leemdragon233@yahoo.com>
2020-10-07 17:48:08 +0800

    init in 10.7

--- src/main/java/sustech/edu/phantom/dboj/DbojApplication.java
+++ src/main/java/sustech/edu/phantom/dboj/DbojApplication.java
@@ -2,7 +2,9 @@ package sustech.edu.phantom.dboj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 
+@Controller
 @SpringBootApplication
 public class DbojApplication {
 
