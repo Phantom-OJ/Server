e35582b86ea4001fdd0692fed8542c0523fe2999
lethal233 <leemdragon233@yahoo.com>
2020-11-17 01:35:03 +0800

    update SA controller

---
+++ src/main/java/sustech/edu/phantom/dboj/controller/SAController.java
@@ -0,0 +1,9 @@
+package sustech.edu.phantom.dboj.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sa")
+public class SAController {
+}
