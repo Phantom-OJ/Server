ee260f3035662623fb7065ac5d24bb2d3d018a8f
lethal233 <leemdragon233@yahoo.com>
2020-11-17 01:35:13 +0800

    update test cases

--- src/test/java/sustech/edu/phantom/dboj/DbojApplicationTests.java
+++ src/test/java/sustech/edu/phantom/dboj/DbojApplicationTests.java
@@ -1,5 +1,6 @@
 package sustech.edu.phantom.dboj;
 
+import com.google.gson.Gson;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mybatis.spring.annotation.MapperScan;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import sustech.edu.phantom.dboj.form.CodeForm;
+import sustech.edu.phantom.dboj.form.Pagination;
+import sustech.edu.phantom.dboj.service.AssignmentService;
 import sustech.edu.phantom.dboj.service.UserService;
 
 import java.sql.Timestamp;
@@ -19,9 +22,22 @@ public class DbojApplicationTests {
     @Autowired
     UserService userService;
 
+    @Autowired
+    AssignmentService assignmentService;
+
     @Test
     public void test1() {
-        CodeForm codeForm = CodeForm.builder().code("ssss").submitTime(new Timestamp(7892749L)).build();
-        System.out.println(codeForm.getSubmitTime());
+//        CodeForm codeForm = CodeForm.builder().code("ssss").submitTime(new Timestamp(7892749L)).build();
+//        System.out.println(codeForm.getSubmitTime());
+    }
+    @Test
+    public void test2(){
+        Pagination pagination = new Pagination();
+        pagination.setStart(1);
+        pagination.setEnd(2);
+        pagination.setParameters();
+        Gson gson = new Gson();
+        String json = gson.toJson(assignmentService.getAssignmentList(pagination));
+        System.out.println(json);
     }
 }
