e1e02e0ee3c55b1dd5b525f8b07d99324a59ca6c
lethal233 <leemdragon233@yahoo.com>
2020-11-21 02:04:45 +0800

    combine with spring security

---
+++ src/main/java/sustech/edu/phantom/dboj/config/MyAuthenticationFilter.java
@@ -0,0 +1,50 @@
+package sustech.edu.phantom.dboj.config;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+public class MyAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        if (!"POST".equals(request.getMethod())) {
+            throw new AuthenticationServiceException("Authentication method not supported: " + request.getMethod());
+        }
+        if (request.getContentType().equals(MediaType.APPLICATION_JSON_VALUE)) {
+            String username = null;
+            String password = null;
+            try {
+                Map<String, String> map = new ObjectMapper().readValue(request.getInputStream(), Map.class);
+                username = map.get("username");
+                password = map.get("password");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (username == null) {
+                username = "";
+            }
+
+            if (password == null) {
+                password = "";
+            }
+
+            username = username.trim();
+            UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(username, password);
+            this.setDetails(request, authRequest);
+            return this.getAuthenticationManager().authenticate(authRequest);
+        }
+        return super.attemptAuthentication(request, response);
+    }
+}
--- src/main/java/sustech/edu/phantom/dboj/config/SecurityConfig.java
+++ src/main/java/sustech/edu/phantom/dboj/config/SecurityConfig.java
@@ -1,7 +1,82 @@
 package sustech.edu.phantom.dboj.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import sustech.edu.phantom.dboj.service.UserService;
+
+import javax.servlet.Filter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
-public class SecurityConfig{
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserService userService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.cors().disable();
+        http.authorizeRequests()
+                .antMatchers("/api/login").permitAll()
+//                .anyRequest().authenticated()
+                .and()
+                .addFilterAt(myAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .formLogin()
+                .loginProcessingUrl("/api/login")
+                .permitAll();
+    }
+
+    private MyAuthenticationFilter myAuthenticationFilter() throws Exception {
+        MyAuthenticationFilter filter = new MyAuthenticationFilter();
+        filter.setAuthenticationManager(super.authenticationManagerBean());
+        filter.setFilterProcessesUrl("/api/login");
+        filter.setAuthenticationSuccessHandler((httpServletRequest, httpServletResponse, authentication) -> {
+            httpServletResponse.setContentType("application/json;charset=utf-8");
+            PrintWriter out = httpServletResponse.getWriter();
+            Map<String, Object> map = new HashMap<>();
+            map.put("status", 200);
+            map.put("msg", authentication.getPrincipal());
+            out.flush();
+            out.close();
+        });
+        return filter;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    RoleHierarchy roleHierarchy() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        String hierarchy = "ROLE_TEACHER > ROLE_SA \n ROLE_SA > ROLE_STUDENT";
+        roleHierarchy.setHierarchy(hierarchy);
+        return roleHierarchy;
+    }
 }
