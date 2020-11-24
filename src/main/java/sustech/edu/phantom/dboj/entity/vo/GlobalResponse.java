541e537b20e65c71316949f81f0a87530abff17e
lethal233 <leemdragon233@yahoo.com>
2020-11-25 01:35:34 +0800

    [update] update GlobalResponse.java

--- src/main/java/sustech/edu/phantom/dboj/entity/vo/GlobalResponse.java
+++ src/main/java/sustech/edu/phantom/dboj/entity/vo/GlobalResponse.java
@@ -15,8 +15,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class GlobalResponse<T> {
-    private transient final static String SUCCESS = "success";
-    private transient final static String FAIL = "fail";
+    public transient final static String SUCCESS = "success";
+    public transient final static String FAIL = "not found";
+    public transient final static String USERNAME_OR_PASSWORD_ERROR = "Username or password error";
+    public transient final static String ACCESS_FORBIDDEN = "forbidden";
+    public transient final static String USERNAME_NOT_FOUND = "username not found";
+    public transient final static String LOGIN_FAILED = "logging in failed";
+    public transient final static String VERIFICATION_CODE_NOT_MATCHED = "Verification code is not matched";
+    public transient final static String SERVER_ERROR = "Internal Server Error";
+
     private String msg;
     private T data;
 }
