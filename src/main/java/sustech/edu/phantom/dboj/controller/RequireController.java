50e35ab0e92f652daf85016e26b62fccbf179c8b
lethal233 <11811407@mail.sustech.edu.cn>
2020-12-05 01:40:45 +0800

    [update] update mvc

---
+++ src/main/java/sustech/edu/phantom/dboj/controller/RequireController.java
@@ -0,0 +1,50 @@
+package sustech.edu.phantom.dboj.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import sustech.edu.phantom.dboj.entity.Group;
+import sustech.edu.phantom.dboj.entity.User;
+import sustech.edu.phantom.dboj.entity.enumeration.ResponseMsg;
+import sustech.edu.phantom.dboj.entity.response.GlobalResponse;
+import sustech.edu.phantom.dboj.service.GroupService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/require")
+@Slf4j
+public class RequireController {
+
+    @Autowired
+    GroupService groupService;
+
+    @RequestMapping(value = "/group", method = RequestMethod.GET)
+    public ResponseEntity<GlobalResponse<List<Group>>> getGroups() {
+        ResponseMsg msg;
+        List<Group> data = null;
+        try {
+            User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            if (user.containSomePermission("view all groups")) {
+                try {
+                    data = groupService.getAllGroups();
+                    msg = ResponseMsg.OK;
+                } catch (Exception e) {
+                    //TODO:log报错信息
+                    log.error("");
+                    msg = ResponseMsg.INTERNAL_SERVER_ERROR;
+                }
+            } else {
+                msg = ResponseMsg.FORBIDDEN;
+            }
+        } catch (ClassCastException e) {
+            //TODO:log.error
+            msg = ResponseMsg.UNAUTHORIZED;
+        }
+        return new ResponseEntity<>(GlobalResponse.<List<Group>>builder().data(data).msg(msg.getMsg()).build(), msg.getStatus());
+    }
+}
--- src/main/java/sustech/edu/phantom/dboj/service/GroupService.java
+++ src/main/java/sustech/edu/phantom/dboj/service/GroupService.java
@@ -1,5 +1,6 @@
 package sustech.edu.phantom.dboj.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,16 +9,23 @@ import sustech.edu.phantom.dboj.mapper.GroupMapper;
 
 import java.util.ArrayList;
 import java.util.List;
+
 /**
  * @author Lori
  */
 @Service
 @Transactional(rollbackFor = Exception.class)
+@Slf4j
 public class GroupService {
     @Autowired
     GroupMapper groupMapper;
 
-    public List<Group> getGroupList(ArrayList<Integer> a){
+    public List<Group> getGroupList(ArrayList<Integer> a) {
         return groupMapper.getGroups(a);
+    }
+
+    public List<Group> getAllGroups() {
+
+        return groupMapper.getAllGroups();
     }
 }
