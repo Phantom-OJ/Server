b1b97c76af5f40564f644deffbdb9f35c381e473
lethal233 <11811407@mail.sustech.edu.cn>
2020-12-06 13:02:16 +0800

    [new] add aop class

---
+++ src/main/java/sustech/edu/phantom/dboj/aspect/ServiceAspectLog.java
@@ -0,0 +1,11 @@
+package sustech.edu.phantom.dboj.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@Aspect
+public class ServiceAspectLog {
+}
