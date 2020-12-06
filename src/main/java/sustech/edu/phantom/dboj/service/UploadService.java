b2784d22853ede74e6cc062474d6b9613dfc11bc
lethal233 <leemdragon233@yahoo.com>
2020-12-07 01:35:52 +0800

    [update] update service

--- src/main/java/sustech/edu/phantom/dboj/service/UploadService.java
+++ src/main/java/sustech/edu/phantom/dboj/service/UploadService.java
@@ -5,9 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.interceptor.TransactionAspectSupport;
-import sustech.edu.phantom.dboj.entity.Assignment;
-import sustech.edu.phantom.dboj.entity.JudgePoint;
-import sustech.edu.phantom.dboj.entity.Problem;
+import sustech.edu.phantom.dboj.entity.*;
 import sustech.edu.phantom.dboj.form.upload.UploadAnnouncementForm;
 import sustech.edu.phantom.dboj.form.upload.UploadAssignmentForm;
 import sustech.edu.phantom.dboj.form.upload.UploadJudgePointForm;
@@ -62,9 +60,9 @@ public class UploadService {
         return flag != 0;
     }
 
-    public boolean saveJudgeDB(List<String> list) {
+    public boolean saveJudgeDB(JudgeDatabase judgeDatabase) {
         try {
-            int a = judgeDatabaseMapper.saveJudgeDatabase(list);
+            int a = judgeDatabaseMapper.saveJudgeDatabase(judgeDatabase);
             if (a > 0) {
                 log.info("Insert " + a + " records into judge database");
                 return true;
@@ -78,10 +76,34 @@ public class UploadService {
             return false;
         }
     }
-
-    public boolean saveJudgeScript(List<String> list) {
+    public JudgeDatabase selOneDB(int id) {
+        JudgeDatabase j = null;
         try {
-            int a = judgeScriptMapper.saveJudgeScript(list);
+            j = judgeDatabaseMapper.selJudgeDatabaseById(id);
+        } catch (Exception e) {
+            log.error("something wrong with selOneDB");
+        }
+        return j;
+    }
+
+
+    /**
+     * 获取所有的judge database信息
+     * @return 所有的judge database
+     */
+    public List<JudgeDatabase> getAllJudgeDB() {
+        List<JudgeDatabase> list = null;
+        try {
+            list = judgeDatabaseMapper.getAllJudgeDB();
+        } catch (Exception e) {
+            log.error("something wrong with getAllJudgeDB");
+        }
+        return list;
+    }
+
+    public boolean saveJudgeScript(JudgeScript judgeScript) {
+        try {
+            int a = judgeScriptMapper.saveJudgeScript(judgeScript);
             if (a > 0) {
                 log.info("Insert " + a + " records into judge script");
                 return true;
@@ -95,9 +117,29 @@ public class UploadService {
             return false;
         }
     }
+    public List<JudgeScript> getAllJudgeScript() {
+        List<JudgeScript> list = null;
+        try {
+            list = judgeScriptMapper.getAllJudgeScript();
+        } catch (Exception e) {
+            log.error("something wrong with getAllJudgeDB");
+        }
+        return list;
+    }
+
+    public JudgeScript selOneJScript(int id) {
+        JudgeScript j = null;
+        try {
+            j = judgeScriptMapper.selOneJudgeScript(id);
+        } catch (Exception e) {
+            log.error("something wrong with selOneJScript");
+        }
+        return j;
+    }
 
     /**
      * 添加整个assignment是一个整体，所以只要其中一个出现了问题就全部rollback
+     *
      * @param form 上传作业的表单
      * @return 是否添加成功
      */
@@ -117,7 +159,6 @@ public class UploadService {
                 int aid = a.getId();
                 for (UploadProblemForm p : form.getUploadProblemFormList()
                 ) {
-                    //TODO: tag not set
                     Problem tmp = Problem
                             .builder()
                             .assignmentId(aid)
@@ -131,7 +172,6 @@ public class UploadService {
                             .status(a.getStatus())
                             .type(p.getType())
                             .build();
-
                     if (problemMapper.saveProblem(tmp) > 0) {
                         List<Integer> list = p.getTagList();
                         if (tagMapper.saveOneProblemTags(list, tmp.getId()) == 0) {
@@ -162,7 +202,6 @@ public class UploadService {
             log.error("Something wrong with saving assignment.");
             return false;
         }
-
         return true;
     }
 }
