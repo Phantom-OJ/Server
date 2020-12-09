93fc1f17174c94683ca6b3485423368e81abf079
lethal233 <leemdragon233@yahoo.com>
2020-12-09 19:20:23 +0800

    [log] add logging configuration

--- src/main/java/sustech/edu/phantom/dboj/aspect/WebAspectLog.java
+++ src/main/java/sustech/edu/phantom/dboj/aspect/WebAspectLog.java
@@ -1,11 +1,14 @@
 package sustech.edu.phantom.dboj.aspect;
 
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 @Slf4j
@@ -13,10 +16,36 @@ import org.springframework.stereotype.Component;
 public class WebAspectLog {
 
     @Pointcut("execution(public * sustech.edu.phantom.dboj.controller..*.*(..))")
-    public void Pointcut(){}
+    public void Pointcut() {
+    }
 
-    @AfterThrowing(value="Pointcut()",throwing="e")
-    public void afterReturningMethod(JoinPoint joinPoint, NumberFormatException e){
-        log.info("调用了异常通知 in aspect" + e.getMessage());
+    @Around("Pointcut()")
+    public Object Around(ProceedingJoinPoint pjp) throws Throwable {
+        Map<String, Object> data = new HashMap<>();
+        //获取目标类名称
+        String clazzName = pjp.getTarget().getClass().getName();
+        //获取目标类方法名称
+        String methodName = pjp.getSignature().getName();
+
+        //记录类名称
+        data.put("clazzName", clazzName);
+        //记录对应方法名称
+        data.put("methodName", methodName);
+        //记录请求参数
+        data.put("params", pjp.getArgs());
+        //开始调用时间
+        // 计时并调用目标函数
+        long start = System.currentTimeMillis();
+        Object result = pjp.proceed();
+        Long time = System.currentTimeMillis() - start;
+
+        //记录返回参数
+        data.put("result", result);
+
+        //设置消耗总时间
+        data.put("consumeTime", time);
+        log.info(data.toString());
+
+        return result;
     }
 }
