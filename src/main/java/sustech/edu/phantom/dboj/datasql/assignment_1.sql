d80666c3515b56532a9f06cdf61e2da486b794f9
Tloops <11813207@mail.sustech.edu.cn>
2020-12-10 11:47:19 +0800

    assignment1 test data

--- src/main/java/sustech/edu/phantom/dboj/datasql/assignment_1.sql
+++ src/main/java/sustech/edu/phantom/dboj/datasql/assignment_1.sql
@@ -9,8 +9,8 @@ All the questions and the sample result sets in this assignment are based on **f
 - You need submit “.sql” files for these six questions. The name of each “.sql” file should be q1, q2, q3, q4, q5, q6 respectively to represent these six questions.
 - Do not forget to add ‘;’ in the end of each query.
 - Do not compress them into a folder, please submit them directly. Please submit those queries into **sakai website** as soon as possible, so that you can get chance to receive feedback before deadline. <span style="color:red">After the deadline, we will check the assignment automatically by a script and then given your grade, at that time, any argument about your grade of this assignment will not be accepted.</span>',
-        1606798800,
-        1608440400,
+        1606798800000,
+        1608440400000,
         'public',
         100,
         ''
