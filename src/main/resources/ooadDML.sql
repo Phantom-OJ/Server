108a0bfc29b2e3ff48429261821e451ba833bb22
lethal233 <leemdragon233@yahoo.com>
2020-12-10 13:00:57 +0800

    [SQL] DML

--- src/main/resources/ooadDML.sql
+++ src/main/resources/ooadDML.sql
@@ -70,23 +70,23 @@ VALUES (1, 1),
 
 -- SELECT setval('problem_id_seq',0,true) FROM public.problem;
 INSERT INTO public.judge_point (problem_id, before_sql, after_sql, judge_script_id, answer, judge_database_id,
-                                valid)
+                                valid,dialect)
 VALUES (1, null, null, 1, '"title|country|year_released"
 "The Adjuster|ca|1991"
 "The Commitments|ie|1991"
 "The Miracle|gb|1991"
- ', 1, true);
+ ', 1, true,'pgsql');
 INSERT INTO public.judge_point (problem_id, before_sql, after_sql, judge_script_id, answer, judge_database_id,
-                                valid)
+                                valid,dialect)
 VALUES (1,
         'insert into movies (movieid, title, country, year_released, runtime) values (9539, ''The Sliver Bullet'', ''us'', 1991, 999);insert into movies (movieid, title, country, year_released, runtime) values (9540, ''The Gold Bullet'', ''cn'', 1991, 999);insert into movies (movieid, title, country, year_released, runtime) values (9541, ''The Bronze Bullet'', ''cn'', 1992, 999);',
         null, 1, '"title|country|year_released"
 "The Adjuster|ca|1991"
 "The Commitments|ie|1991"
 "The Miracle|gb|1991"
-"The Gold Bullet|cn|1991', 1, true);
+"The Gold Bullet|cn|1991', 1, true,'pgsql');
 INSERT INTO public.judge_point (problem_id, before_sql, after_sql, judge_script_id, answer, judge_database_id,
-                                valid)
+                                valid,dialect)
 VALUES (2, null, 'CREATE TRIGGER people_trigger BEFORE INSERT on people FOR EACH ROW EXECUTE PROCEDURE valid_check();
 INSERT INTO people VALUES (''130631190002140071'',''张三'');INSERT INTO people VALUES (''130631190002140079'',''张三'');INSERT INTO people VALUES (''44190019971024031X'',''李四'');INSERT INTO people VALUES (''441900199710240311'',''李四'');INSERT INTO people VALUES (''441881202011116667'',''王五'');INSERT INTO people VALUES (''44188120201111666X'',''王五'');INSERT INTO people VALUES (''001122192303127898'',''Lisa'');INSERT INTO people VALUES (''001122192303127892'',''Lisa'');INSERT INTO people VALUES (''110108186701310096'',''乃万'');INSERT INTO people VALUES (''110108186701310090'',''乃万'');INSERT INTO people VALUES (''130209192705264310'',''风清扬'');INSERT INTO people VALUES (''130209192705264314'',''风清扬'');INSERT INTO people VALUES (''140926193309253012'',''慕容复'');INSERT INTO people VALUES (''14092619330925301X'',''慕容复'');INSERT INTO people VALUES (''141022197702022098'',''虚竹'');INSERT INTO people VALUES (''141022197702022091'',''虚竹'');INSERT INTO people VALUES (''150223197604312209'',''张小龙'');INSERT INTO people VALUES (''15022319760431220X'',''张小龙'');INSERT INTO people VALUES (''150223199902292196'',''pony'');INSERT INTO people VALUES (''15022319990229219X'',''pony'');INSERT INTO people VALUES (''150223200002292137'',''martin'');INSERT INTO people VALUES (''15022320000229213X'',''martin'');INSERT INTO people VALUES (''150400202001002228'',''Tom'');INSERT INTO people VALUES (''15040020200100222X'',''Tom'');INSERT INTO people VALUES (''150600201802291118'',''张无忌'');INSERT INTO people VALUES (''15060020180229111X'',''张无忌'');INSERT INTO people VALUES (''210106188802331902'',''赵敏'');INSERT INTO people VALUES (''21010618880233190X'',''赵敏'');INSERT INTO people VALUES (''210106999903040011'',''段誉'');INSERT INTO people VALUES (''21010699990304001X'',''段誉'');INSERT INTO people VALUES (''210505197503210043'',''李秋水'');INSERT INTO people VALUES (''21050519750321004X'',''李秋水'');INSERT INTO people VALUES (''210700197404060026'',''李沧海'');INSERT INTO people VALUES (''21070019740406002X'',''李沧海'');INSERT INTO people VALUES (''210700177707210898'',''逍遥子'');INSERT INTO people VALUES (''210700177707210890'',''逍遥子'');INSERT INTO people VALUES (''210902197107210992'',''无崖子'');INSERT INTO people VALUES (''21090219710721099X'',''无崖子'');INSERT INTO people VALUES (''310117192507121990'',''郭靖'');INSERT INTO people VALUES (''31011719250712199X'',''郭靖'');INSERT INTO people VALUES (''310117202506008888'',''黄蓉'');INSERT INTO people VALUES (''31011720250600888X'',''黄蓉'');INSERT INTO people VALUES (''140106199903310217'',''小龙女'');INSERT INTO people VALUES (''14010619990331021X'',''小龙女'');INSERT INTO people VALUES (''320206199809230041'',''杨过'');INSERT INTO people VALUES (''320206199809230042'',''杨过'');INSERT INTO people VALUES (''320206299907051985'',''杨逍'');INSERT INTO people VALUES (''320206299907051983'',''杨逍'');INSERT INTO people VALUES (''421122321011121976'',''韦一笑'');INSERT INTO people VALUES (''421122321011121978'',''韦一笑'');select id,name,address,birthday from people;',
         2, '"id|name|address|birthday"
@@ -105,14 +105,13 @@ INSERT INTO people VALUES (''130631190002140071'',''张三'');INSERT INTO people
 "140106199903310217|小龙女|山西省,太原市,迎泽区|19990331"
 "320206199809230041|杨过|江苏省,无锡市,惠山区|19980923"
 "320206299907051985|杨逍|江苏省,无锡市,惠山区|29990705"
-"421122321011121976|韦一笑|湖北省,黄冈市,红安县|32101112"', 2, true);
-INSERT INTO public.judge_database (database_url, valid, keyword)
-VALUES ('jdbc:postgresql://47.102.221.90:12002/postgres?allowMultiQueries=true&currentSchema=public&reWriteBatchedInserts=true
-', true, 'k1');
-INSERT INTO public.judge_database (database_url, valid, keyword)
-VALUES (
-        'jdbc:postgresql://47.102.221.90:12002/postgres?allowMultiQueries=true&currentSchema=trigger_db&reWriteBatchedInserts=true',
-        true, 'k2');
+"421122321011121976|韦一笑|湖北省,黄冈市,红安县|32101112"', 2, true,'pgsql');
+INSERT INTO public.judge_database (database_url, valid, keyword,dialect)
+VALUES ('{"host":"localhost","start_port":12000,"end_port":12010,"image_id":12345}
+', true, 'k1','pgsql');
+INSERT INTO public.judge_database (database_url, valid, keyword,dialect)
+VALUES ('{"host":"localhost","start_port":12000,"end_port":12010,"image_id":12345}',
+        true, 'k2','pgsql');
 INSERT INTO public.problem (assignment_id, title, description, full_score, space_limit, time_limit, number_submit,
                             number_solve, index_in_assignment, solution, valid, status, type)
 VALUES (1, 'select_test', 'select_test_description', 100, 46, 10000, 14, 5, 1, 'no solution', true, 'public',
@@ -138,52 +137,114 @@ VALUES (1, 'test2', '# Computer Network hw1
 
 **SID: **11811407
 
-## 1
-
-### packet switch
-
 ### circuit switch
 
 The total delay is
 $$
 d_{cs} = \frac{x}{b}+kd+s
-$$
-
-If packet switch is smaller:
-$$
-d_p<d_c\\ \text{i.e.}
-\\
-\frac{x}{b}+kd+(k-1)\cdot \frac{p}{b}<\frac{x}{b}+kd+s
-\\\implies s>\frac{p(k-1)}{b}
 $$',
         20, 55, 33, 7, 2, 2, 'store a markdown', true, 'public', 'select');
+insert into public.problem(id, assignment_id, title, description, full_score, space_limit, time_limit, number_submit,
+                           number_solve, index_in_assignment, solution, type, judge_script_id)
+VALUES (2, 'p2.1', 'd2.1', 20, 256, 3000, 0, 0, 1, '### MySQL
+
+```mysql
+SELECT * FROM t WHERE id = 1;
+```
+
+### PgSQL
+
+```plsql
+SELECT * FROM t WHERE id = 1;
+```
+
+### SQLite
+
+```sqlite
+SELECT * FROM t WHERE id = 1;
+```', 'select'),
+       (2, 'p2.2', 'd2.2', 20, 256, 3000, 0, 0, 2, '### MySQL
+
+```mysql
+SELECT * FROM t WHERE id = 1;
+```
+
+### PgSQL
+
+```plsql
+SELECT * FROM t WHERE id = 1;
+```
+
+### SQLite
+
+```sqlite
+SELECT * FROM t WHERE id = 1;
+```', 'select', 1),
+       (2, 'p2.3', 'd2.3', 20, 256, 3000, 0, 0, 3, '### MySQL
+
+```mysql
+SELECT * FROM t WHERE id = 1;
+```
+
+### PgSQL
+
+```plsql
+SELECT * FROM t WHERE id = 1;
+```
+
+### SQLite
+
+```sqlite
+SELECT * FROM t WHERE id = 1;
+```', 'select', 1),
+       (2, 'p2.4', 'd2.4', 20, 256, 3000, 0, 0, 4, '### MySQL
+
+```mysql
+SELECT * FROM t WHERE id = 1;
+```
+
+### PgSQL
+
+```plsql
+SELECT * FROM t WHERE id = 1;
+```
+
+### SQLite
+
+```sqlite
+SELECT * FROM t WHERE id = 1;
+```', 'select', 1),
+       (2, 'p2.5', 'd2.5', 20, 256, 3000, 0, 0, 5, '### MySQL
+
+```mysql
+SELECT * FROM t WHERE id = 1;
+```
+
+### PgSQL
+
+```plsql
+SELECT * FROM t WHERE id = 1;
+```
+
+### SQLite
+
+```sqlite
+SELECT * FROM t WHERE id = 1;
+```', 'select', 1);
+
 INSERT INTO public.assignment (title, description, start_time, end_time, status, full_score, sample_database_path,
                                valid)
 VALUES ('title1', '# Computer Network hw1
 
-**Name**:黎诗龙
 $$
 d_{pw} = d_t+d_{sf}+d_p = \frac{x}{b}+kd+(k-1)\cdot \frac{p}{b}
 $$
-
-## 2
-
-Since in the description it says that *The handshaking process costs 2RTT before transmitting the file.*, we don''t consider transmitting file during the handshaking.
-
-In my solution, I consider `Mb` as `10**6 bits`.
-
-And in my solution, `0.5RTT` means in the final transmission, I just send the file, regardless of the return message from the receiver.  ',
+',
         1605546219888, 1605546251315, 'Running', 100, '/home/adoj/a1/s1.sql', true);
 
 insert into public.assignment (title, description, start_time, end_time, full_score, sample_database_path)
 VALUES ('title2', 'description2', 1, 2, 100, '/home/adoj/a1/s2.sql');
 
-insert into public.problem(id, assignment_id, title, description, full_score, space_limit, time_limit, number_submit,
-                           number_solve, index_in_assignment, solution, type)
-VALUES (2, 'p2.1', 'd2.1', 20, 256, 3000, 0, 0, 1, 'no solution', 'select'),
-       (2, 'p2.2', 'd2.2', 20, 256, 3000, 0, 0, 2, 'no solution', 'select'),
-       (2, 'p2.3', 'd2.3', 20, 256, 3000, 0, 0, 3, 'no solution', 'select'),
-       (2, 'p2.4', 'd2.4', 20, 256, 3000, 0, 0, 4, 'no solution', 'select'),
-       (2, 'p2.5', 'd2.5', 20, 256, 3000, 0, 0, 5, 'no solution', 'select');
+
 
 commit;
